4eec23a309e327c60ed916f712f94e075c218f9c
Carl Beaverson <Carlbeaverson@gmail.com>
2026-05-23 15:51:17 +1000

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -21,7 +21,7 @@ export default function Seo({ description, title, children }) {
 
   return (
     <>
-      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+      <title>{title ? (defaultTitle ? `${title} | ${defaultTitle}` : title) : defaultTitle}</title>
       <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1' />
       <meta name='description' content={metaDescription} />
       <meta property='og:title' content={title} />
