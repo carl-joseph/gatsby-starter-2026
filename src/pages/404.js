5eb86d1606f8f3ade04e9c573bcb4e910704b84a
Carl Beaverson <Carlbeaverson@gmail.com>
2026-08-10 11:00:17 +1000

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,12 @@ import Layout from "../components/layout"
 export default function NotFoundPage() {
   return (
     <Layout>
-      <h1>404: Not Found</h1>
-      <p>You've just hit a page that doesn&#39;t exist... the sadness.</p>
+      <div className='h-100vh flex'>
+        <div className='flex flex-col gap-20 ma text-center'>
+          <h1>404: Not Found</h1>
+          <p>You've just hit a page that doesn&#39;t exist. Please return home.</p>
+        </div>
+      </div>
     </Layout>
   )
 }
